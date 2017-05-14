2ccc836cdc690e5569c85ada459fbe5ca1b84e71
artistbuddy <artist.buddy@gmail.com>
2017-05-14 21:01:16 +0200

    Done Lesson 7

--- Lesson7/L7_Exercises.playground/Contents.swift
+++ Lesson7/L7_Exercises.playground/Contents.swift
@@ -27,6 +27,12 @@ var espressoForGabrielle = macchiato
 espressoForGabrielle.steamedMilk = .splash
 macchiato.steamedMilk
 
+print(macchiato.steamedMilk)
+print(espressoForGabrielle.steamedMilk)
+
+
+//struct are copied by value so espressoForGabrielle.steamedMilk is a copy that not affect if macchiato.steamedMilk changed
+//class are copied by reference so espressoForGabrielle and macchiato are the same objects
 //: __Problem 2__
 //:
 //: __2a.__
@@ -34,76 +40,88 @@ macchiato.steamedMilk
 //:
 //: __2b.__
 //: Associate an Int value with each finger.
-
+enum Fingers: Int {
+    case thumb = 1
+    case index = 2
+    case middle = 3
+    case ring = 4
+    case pinky = 5
+}
 //: __Problem 3__
 //:
 //: Enum, class, or struct?
 //:
 //: Uncomment the code below and choose whether each type should be an enum, class, or struct.
-//____ Window {
-//    let height: Double
-//    let width: Double
-//    var open: Bool
-//}
+struct Window {
+    let height: Double
+    let width: Double
+    var open: Bool
+}
 
-//____ WritingImplement {
-//    case pen
-//    case pencil
-//    case marker
-//    case crayon
-//    case chalk
-//}
+enum WritingImplement {
+    case pen
+    case pencil
+    case marker
+    case crayon
+    case chalk
+}
 
-//____ Material {
-//    let name: String
-//    let density: Double
-//    let stiffness: Double
-//}
+struct Material {
+    let name: String
+    let density: Double
+    let stiffness: Double
+}
 
 
-//____ Bicycle {
-//    let frame: Material
-//    let weight: Double
-//    let category: String
-//
-//    static var bikeCategories: [String] = ["Road", "Touring", "Mountain", "Commuter", "BMX"]
-//
-//    func lookCool() {
-//        print("Check out my gear-shifters!")
-//    }
-//}
+class Bicycle {
+    let frame: Material
+    let weight: Double
+    let category: String
 
-//____ Cyclist {
-//    var speed: Double
-//    let agility: Double
-//    let bike: Bicycle
-//
-//    var maneuverability: Double {
-//        get {
-//            return agility - speed/5
-//        }
-//    }
-//
-//    init(speed: Double, agility: Double, bike: Bicycle) {
-//        self.speed = speed
-//        self.agility = agility
-//        self.bike = bike
-//    }
-//
-//    func brake() {
-//        speed -= 1
-//    }
-//
-//    func pedalFaster(factor: Double) {
-//        speed * factor
-//    }
-//}
+    static var bikeCategories: [String] = ["Road", "Touring", "Mountain", "Commuter", "BMX"]
+    
+    init(frame: Material, weight: Double, category: String) {
+        self.frame = frame
+        self.weight = weight
+        self.category = category
+    }
 
-//____ Size: String {
-//    case small = "8 ounces"
-//    case medium = "12 ounces"
-//    case large = "16 ounces"
-//}
+    func lookCool() {
+        print("Check out my gear-shifters!")
+    }
+}
+
+class Cyclist {
+    var speed: Double
+    let agility: Double
+    let bike: Bicycle
+
+    var maneuverability: Double {
+        get {
+            return agility - speed/5
+        }
+    }
+
+    init(speed: Double, agility: Double, bike: Bicycle) {
+        self.speed = speed
+        self.agility = agility
+        self.bike = bike
+    }
+
+    func brake() {
+        speed -= 1
+    }
+
+    func pedalFaster(factor: Double) {
+        speed * factor
+    }
+}
+
+enum Size: String {
+    case small = "8 ounces"
+    case medium = "12 ounces"
+    case large = "16 ounces"
+}
 
 //: __Problem 4__
 //:
@@ -117,10 +135,29 @@ macchiato.steamedMilk
 
 //:__4c.__
 //:Include a method. For example, the method tempt() might return or print out an indication of a person being tempted to eat the cookie.
-
+struct Cookie {
+    let flavour: String
+    var minutesSinceRemovalFromOven: Int
+    var delicious: Bool {
+        if minutesSinceRemovalFromOven > 10 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func tempt() {
+        if delicious {
+            print("It's so yummy, dig in!")
+        } else {
+            print("You shouldn't")
+        }
+    }
+}
 //: __4d.__
 //: Create an instance of your Cookie struct and call its method.
-
+let biscuit = Cookie(flavour: "banana", minutesSinceRemovalFromOven: 2)
+biscuit.tempt()
 //: __Problem 5__
 //:
 //: Write a class to represent a listing for a Bed and Breakfast.
@@ -136,3 +173,35 @@ macchiato.steamedMilk
 
 //: __5d.__
 //: Create an instance of your BnBListing class and call one of its methods.
+class BnBListing {
+    enum Quality {
+        case vip, high, standard
+    }
+    
+    enum Category {
+        case bungalow, apartament
+    }
+    
+    let category: Category
+    let quality: Quality
+    var avaiable: Bool
+    
+    init(category: Category, quality: Quality, avaiable: Bool) {
+        self.category = category
+        self.quality = quality
+        self.avaiable = avaiable
+    }
+    
+    func book() {
+        if avaiable {
+            print("Ok, booked!")
+            avaiable = false
+        } else {
+            print("Sorry, too late :(")
+        }
+    }
+}
+
+let miami = BnBListing(category: .apartament, quality: .vip, avaiable: true)
+miami.book()
+miami.avaiable
